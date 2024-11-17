8829cf8ce120d0e47437a4a449cf6ac8e9646ee9
Danny Thibaudeau <cobreti_@hotmail.com>
2024-11-17 17:23:31 -0500

    denoapp cleanup

--- DemoApp/vite.config.ts
+++ DemoApp/vite.config.ts
@@ -3,12 +3,19 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import sassDts from 'vite-plugin-sass-dts'
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    sassDts(),
+    vueJsx(),
+    tsconfigPaths()
+
   ],
   resolve: {
     alias: {
