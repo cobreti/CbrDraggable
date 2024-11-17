0e0e9e6a17cc27dca6f748918b28c767ed83c27f
Danny Thibaudeau <cobreti_@hotmail.com>
2024-11-17 16:32:05 -0500

    fixed build config

--- lib/vite.config.ts
+++ lib/vite.config.ts
@@ -1,7 +1,26 @@
 import {resolve} from 'path';
 import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import sassDts from 'vite-plugin-sass-dts'
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+    plugins: [
+        vue(),
+        sassDts(),
+        vueJsx(),
+        tsconfigPaths()
+    ],
+    optimizeDeps: {
+        esbuildOptions: {
+            tsconfigRaw: {
+                compilerOptions: {
+                    experimentalDecorators: true
+                }
+            }
+        }
+    },
     build: {
         lib: {
             entry: resolve(__dirname, 'src/main.ts'),
